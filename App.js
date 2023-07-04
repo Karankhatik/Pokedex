79136d2cb9c815bc22659d788c52756f60bd9b91
Karan Khatik <karan13699@gmail.com>
2023-07-04 18:21:29 +0530

    Project intial setup with parcel and react - Done

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () => {
+  return (
+    <>
+    <h1>
+        React Project set Up Done
+    </h1>     
+    </>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
